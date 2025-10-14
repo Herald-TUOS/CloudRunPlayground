20b92c59f9973bf0c4233c73317f7299d6672772
Herald-TUOS <h.olakkengil@sheffield.ac.uk>
2025-10-14 16:59:20 +0100

    add config handling and logging

--- src/main.py
+++ src/main.py
@@ -1,6 +1,30 @@
+import logging
+from datetime import datetime
 from config_handler import Config
+from secret_handler import Secrets
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+
+def log_time_now():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 if __name__ == "__main__":
-    config = Config("configs/config.yaml")
-    print(config.config)
+
+    config = Config("configs/config.yaml").config
+    secrets = Secrets("configs/secrets.yaml")
+
+    print("Read config file:", config)
+    print("Read secrets file:", secrets.get_redacted_secrets(secrets.secrets))
+
+    if config["params"]["log_level"] == "DEBUG":
+        logger.debug(f"This is a cloud run {config["params"]["cloud_run_type"]}")
+        logger.debug(f"Current time: {log_time_now()}")
+    elif config.config["params"]["log_level"] == "INFO":
+        logger.debug(f"This is a cloud run {config["params"]["cloud_run_type"]}")
+        logger.info(f"Current time: {log_time_now()}")
---
+++ src/secret_handler.py
@@ -0,0 +1,22 @@
+import yaml
+
+
+class Secrets:
+    def __init__(self, secrets_file):
+        self.secrets_file = secrets_file
+        self.secrets = self.load_secrets()
+
+    def load_secrets(self):
+        with open(self.secrets_file, "r") as file:
+            return yaml.safe_load(file)
+
+    @staticmethod
+    def get_redacted_secrets(secrets):
+        return {
+            key: (
+                "REDACTED"
+                if isinstance(secrets[key], str)
+                else Secrets.get_redacted_secrets(secrets[key])
+            )
+            for key in secrets
+        }
