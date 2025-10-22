fbf220eb5d8c2877df63e3ce87831f22d956d1d8
Herald-TUOS <h.olakkengil@sheffield.ac.uk>
2025-10-22 13:43:38 +0100

    update image used by GCR

--- update_gcr_job.sh
+++ update_gcr_job.sh
@@ -4,7 +4,7 @@ ARGS=(
         # use this service account to run this job
         --service-account=sa-cloudrunplayground@pvlive.iam.gserviceaccount.com
         # Change this image to make new release
-        --image=europe-west2-docker.pkg.dev/pvlive/cloud-run-source-deploy/cloud_run_playground:20251021
+        --image=europe-west2-docker.pkg.dev/pvlive/cloudrunplayground-dockerhub/sheffieldsolar/cloud_run_playground:20251021
         # change secrets version here!
         --update-secrets=/configs/config.yaml=cloudrunplayground_config_yml:1
         --update-secrets=/secrets/secrets.yaml=cloudrunplayground_secrets_yml:1
